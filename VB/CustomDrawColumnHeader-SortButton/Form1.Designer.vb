ca131392f23c30df1c170a510c4f311ad9dfc860
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomDrawColumnHeader-SortButton/Form1.Designer.vb
@@ -0,0 +1,91 @@
+﻿Namespace CustomDrawColumnHeader_SortButton
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+			Me.sortGlyphCollection = New DevExpress.Utils.ImageCollection(Me.components)
+			Me.timer1 = New System.Windows.Forms.Timer(Me.components)
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.sortGlyphCollection, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' gridControl1
+			' 
+			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+			Me.gridControl1.LookAndFeel.SkinName = "Office 2013"
+			Me.gridControl1.LookAndFeel.UseDefaultLookAndFeel = False
+			Me.gridControl1.MainView = Me.gridView1
+			Me.gridControl1.Name = "gridControl1"
+			Me.gridControl1.Size = New System.Drawing.Size(430, 187)
+			Me.gridControl1.TabIndex = 0
+			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+			' 
+			' gridView1
+			' 
+			Me.gridView1.GridControl = Me.gridControl1
+			Me.gridView1.Name = "gridView1"
+'			Me.gridView1.CustomDrawColumnHeader += New DevExpress.XtraGrid.Views.Grid.ColumnHeaderCustomDrawEventHandler(Me.gridView1_CustomDrawColumnHeader)
+			' 
+			' sortGlyphCollection
+			' 
+			Me.sortGlyphCollection.ImageSize = New System.Drawing.Size(11, 11)
+			Me.sortGlyphCollection.ImageStream = (CType(resources.GetObject("sortGlyphCollection.ImageStream"), DevExpress.Utils.ImageCollectionStreamer))
+			Me.sortGlyphCollection.Images.SetKeyName(0, "arrow-up2.png")
+			Me.sortGlyphCollection.Images.SetKeyName(1, "arrow-down2.png")
+			' 
+			' timer1
+			' 
+			Me.timer1.Enabled = True
+'			Me.timer1.Tick += New System.EventHandler(Me.timer1_Tick)
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(430, 187)
+			Me.Controls.Add(Me.gridControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+'			Me.Load += New System.EventHandler(Me.Form1_Load)
+			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.sortGlyphCollection, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private gridControl1 As DevExpress.XtraGrid.GridControl
+		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+		Private sortGlyphCollection As DevExpress.Utils.ImageCollection
+		Private WithEvents timer1 As System.Windows.Forms.Timer
+	End Class
+End Namespace
+
